d47b4c949e5c44ca0450848531593ea53ed33f89
DVitsme <derrick.valentine@gmail.com>
2020-09-24 17:54:07 -0400

    meant to remove old server need to rename server2

--- server/config/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', 'd8c0fffc5dd3e4bc5664c447fdd7a63b'),
-    },
-  },
-});
--- server/extensions/users-permissions/config/jwt.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  jwtSecret: process.env.JWT_SECRET || '2afc41cc-8483-451c-a6b4-feb78be4df34'
-};
