f0c7eb922addb5dc2ea780cbcecec6c4314f5581
DVitsme <derrick.valentine@gmail.com>
2021-03-30 09:09:02 -0400

    reseting project to reflect updated hosting

--- client/contexts/FetctData.js
+++
@@ -1,5 +0,0 @@
-import { createContext } from 'react';
-
-const FetchData = createContext(null);
-
-export default FetchData;
--- server2/extensions/users-permissions/config/jwt.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  jwtSecret: process.env.JWT_SECRET || 'cb648fd5-68d1-40cc-83fc-ed310fe2bdd7'
-};
